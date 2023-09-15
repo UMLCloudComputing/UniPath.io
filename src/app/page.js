16c377fb50e886af3c728201553c65ac7638181f
Lapse <lapse@example.com>
2023-09-14 21:03:25 -0400

    HELLL YEAH

--- src/app/page.js
+++ src/app/page.js
@@ -86,7 +86,7 @@ export default function Home() {
             Deploy <span>-&gt;</span>
           </h2>
           <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+            LOLOOL YOUGOT TROLLED NOW GIMME UR MONEY $$$
           </p>
         </a>
       </div>
