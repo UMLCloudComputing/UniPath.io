cf959a478875ca6468f8ea7bd15763758d376d35
Yibo Li <lslsls0001@gmail.com>
2023-09-21 19:35:32 -0400

    update src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -59,7 +59,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Study <span>-&gt;</span>
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
