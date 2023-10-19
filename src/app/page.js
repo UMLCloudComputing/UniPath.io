c51be911e66591d78f7bd95f63f3c53f288d6566
AmazingConnie <rmallu21@gmail.com>
2023-10-19 18:12:18 -0400

    Added info to page.js about the project.

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      Hi, I'm a page
+      Hi, I'm a page. We're off to a wonderful journey here on UniPath.io!
     </main>
   )
 }
