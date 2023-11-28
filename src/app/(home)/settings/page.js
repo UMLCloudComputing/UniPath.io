c79dddeefaba984eb3389ac93a14c272af5e6521
Noah <145724750+noahshayne@users.noreply.github.com>
2023-11-27 22:55:27 -0500

    Noah/add support and settings pages (#68)
    * Added settings and support pages. They are both linked to Drawer.js
    
    * Removed unnecessary imports for now
    
    ---------
    
    Co-authored-by: Noah Shayne <noah.shayne@gmail.com>

---
+++ src/app/(home)/settings/page.js
@@ -0,0 +1,21 @@
+// React
+import * as React from 'react';
+
+// Material UI
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+
+
+export default function SettingsPage() {
+    return (
+        <Box>
+            <Typography variant="body1" gutterBottom>
+                Settings page
+            </Typography>
+        </Box>
+    );
+    }
+
+  
---
+++ src/app/(home)/support/page.js
@@ -0,0 +1,20 @@
+// React
+import * as React from 'react';
+
+// Material UI
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+
+
+export default function SupportPage() {
+    return (
+        <Box>
+            <Typography variant="body1" gutterBottom>
+                Support page
+            </Typography>
+        </Box>
+    );
+    }
+
+  
