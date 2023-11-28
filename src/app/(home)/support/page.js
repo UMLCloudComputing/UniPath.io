569275f6943e82d720c3a80562a4121da432e721
Noah Shayne <noah.shayne@gmail.com>
2023-11-27 21:11:55 -0500

    Removed unnecessary imports for now

--- src/app/(home)/support/page.js
+++ src/app/(home)/support/page.js
@@ -2,19 +2,8 @@
 import * as React from 'react';
 
 // Material UI
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 
