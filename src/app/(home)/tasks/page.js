2cfc150780874a1053dd541ce18a302fa0c17c98
Martin <44274319+MartinMarwad@users.noreply.github.com>
2024-01-10 22:25:57 -0500

    Revert "Implemented more the Tasks page groundwork (UI, working wireframe, etc)" (#91)
    Co-authored-by: Gurpranked <93165798+Gurpranked@users.noreply.github.com>

--- src/app/(home)/tasks/page.js
+++ src/app/(home)/tasks/page.js
@@ -1,40 +1,20 @@
-"use client";
-
-// Component Imports
-import Column from './Column';
-
-// Data Imports
-import { initialData } from './initialData';
-
-// DND
-// Not used yet
-import { DragDropContext } from 'react-beautiful-dnd';
 
 // React
 import * as React from 'react';
 
-// MUI
-import { Typography } from '@mui/material';
+// Material UI
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 
-
-export default function Lists() {
-    //   const [checked, setChecked] = React.useState([1]);
-    //   const [impChecked, setImpChecked] = React.useState([1]);
-    const data = initialData;
-
+// Component: TasksPage
+export default function TasksPage() {
     return (
-        data.columnOrder.map(columnId => {
-            const column = data.columns[columnId];
-            const tasks = column.tasksIds.map(taskId => data.tasks[taskId]);
-            const title = column.title;
-            return (
-                <div style={{ display: 'inline-flex', flexDirection: 'row', rowGap: '20px' }}>
-                    <div style={{ display: 'inline-flex', flexDirection: 'column', columnGap: '20px' }}>
-                        <Typography variant='h4'>{title}</Typography>
-                        <Column tasks={tasks} title={title} />
-                    </div>
-                </div>
-            );
-        }));
-};
+        <Box>
+            <Typography variant="body1" gutterBottom>
+                Tasks Page
+            </Typography>
+        </Box>
+    );
+}
