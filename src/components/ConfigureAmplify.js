56e9a67785adc7c737ea9f286ca94aed883a6c58
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-01-11 19:16:02 -0500

    Migrated the dependecies to Amplify 2.0.
    Co-authored-by: Martin <MartinMarwad@users.noreply.github.com>
    Co-authored-by: Mallu, Rohan <Rohan_Mallu@student.uml.edu>
    Co-authored-by: lapse <ultralapse@users.noreply.github.com>
    Co-authored-by: ReesCodes <ReesCodes@users.noreply.github.com>

---
+++ src/components/ConfigureAmplify.js
@@ -0,0 +1,12 @@
+// components/ConfigureAmplify.tsx
+"use client";
+
+
+import config from "MAIN/amplifyconfiguration.json";
+import { Amplify } from "aws-amplify";
+
+Amplify.configure(config, { ssr: true });
+
+export default function ConfigureAmplifyClientSide() {
+    return null;
+}
