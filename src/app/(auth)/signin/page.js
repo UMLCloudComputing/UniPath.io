628138012eaf633bf0eccae96de1d5c9392a6c9f
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2024-01-24 16:34:37 -0500

    Sign in page now properly authenticates with AWS.
    I added an 'incorrect password' message but I cant do CSS for my life

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -1,96 +1,150 @@
 "use client";
-// React 
-import * as React from 'react';
+// React
+import * as React from "react";
 
 /* Original Template: 
 https://github.com/mui/material-ui/blob/v5.14.18/docs/data/material/getting-started/templates/sign-in/SignIn.js*/
 
 // Material UI
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// Next.js
+import { useRouter } from "next/navigation";
+
+// AWS
+import { signIn } from "aws-amplify/auth";
 
 function Copyright(props) {
-    return(
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-            {'Copyright © '}
+    return (
+        <Typography
+            variant="body2"
+            color="text.secondary"
+            align="center"
+            {...props}
+        >
+            {"Copyright © "}
             <Link color="inherit" href="https://mui.com/">
                 UniPath.io
-            </Link>{' '}
+            </Link>{" "}
             {new Date().getFullYear()}
-            {'.'}
+            {"."}
         </Typography>
     );
 }
 
 const defaultTheme = createTheme();
 
-// Page: SignInPage
+// Page: SignInPage;
 export default function SignInPage() {
-    const handleSubmit = (event) => {
+    const router = useRouter();
+    const [showIncorrectEmailOrPassword, setShowIncorrectEmailOrPassword] =
+        React.useState(false);
+    const handleSubmit = async (event) => {
         event.preventDefault();
         const data = new FormData(event.currentTarget);
         console.log({
-            email: data.get('email'),
-            password: data.get('password'),
+            email: data.get("email"),
+            password: data.get("password"),
         });
+        try {
+            const { isSignedIn, nextStep } = await signIn({
+                username: data.get("email"),
+                password: data.get("password"),
+            });
+            switch (
+                nextStep.signInStep // this is where we could reroute in the future to SMS confirmation codes or password reset, etc.
+            ) {
+                case "DONE":
+                    router.push("/home");
+            }
+        } catch ({ name, message }) {
+            console.log("error signing in", name, message);
+
+            if (name === "NotAuthorizedException") {
+                setShowIncorrectEmailOrPassword(true);
+            }
+        }
     };
+
     return (
         <ThemeProvider theme={defaultTheme}>
             <Container component="main" maxWidth="xs">
                 <CssBaseline />
-                <Box sx={{
-                    marginTop: 8,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'center'
-                }}>
-                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main '}}>
+
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "center",
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: "secondary.main " }}>
                         <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
                         Sign in
                     </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField 
-                          margin="normal"
-                          required
-                          fullWidth
-                          id="email"
-                          label="Email Address"
-                          name="email"
-                          autoComplete="email"
-                          autoFocus
+                    <Box
+                        component="form"
+                        onSubmit={handleSubmit}
+                        noValidate
+                        sx={{ mt: 1 }}
+                    >
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
                         />
-                        <TextField 
-                          margin="normal"
-                          required
-                          fullWidth
-                          name="password"
-                          label="Password"
-                          type="password"
-                          id="password"
-                          autoComplete="current-password"
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
                         />
-                        <FormControlLabel 
-                          control={<Checkbox value="remember" color="primary" />}
-                          label="Remember me"
+                        {showIncorrectEmailOrPassword ? (
+                            <Box
+                                margin="normal"
+                                id="email-or-pass-error"
+                                name="email-or-pass-error"
+                                color={"red"}
+                            >
+                                Incorrect email or password
+                            </Box>
+                        ) : null}
+                        <FormControlLabel
+                            control={
+                                <Checkbox value="remember" color="primary" />
+                            }
+                            label="Remember me"
                         />
-                        <Button 
-                          type="submit"
-                          fullWidth
-                          variant="contained"
-                          sx={{ mt: 3, mb: 2 }}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
                             Sign in
                         </Button>
                         <Grid container>
