a91fa36c084ab2526540236fb2018faae902e66a
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2024-01-24 22:01:48 -0500

    refactored app bar into a single component and added condition to only show certain elements on certain pages

--- src/app/(home)/home/page.js
+++ src/app/(home)/home/page.js
@@ -1,30 +1,31 @@
-
+"use client";
 // React
-import * as React from 'react';
-import Link from 'next/link';
+import * as React from "react";
+import Link from "next/link";
 
 // Material UI
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Unstable_Grid2';
-import Drawer from '@mui/material/Drawer';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Alert from '@mui/material/Alert';
-import AlertTitle from '@mui/material/AlertTitle';
-import MediaCard from '@/components/MediaCard';
-
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import MediaCard from "@/components/MediaCard";
 
 // Component: HomePage
 export default function HomePage() {
     return (
-        <Box sx={{ display: 'flex' }}>
+        <Box sx={{ display: "flex" }}>
             <Typography variant="body1" gutterBottom>
                 Home Page
             </Typography>
 
             <Typography variant="body1" gutterBottom>
-                Nothing here yet, go to the <Link href="/pathways">Pathways</Link> page or the <Link href="/tasks">Tasks</Link> page instead.
+                Nothing here yet, go to the{" "}
+                <Link href="/pathways">Pathways</Link> page or the{" "}
+                <Link href="/tasks">Tasks</Link> page instead.
             </Typography>
         </Box>
     );
--- src/app/(landing)/page.js
+++ src/app/(landing)/page.js
@@ -12,6 +12,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Container from "@mui/material/Container";
 import SchoolIcon from "@mui/icons-material/School";
+import AppBarComponent from "@/components/AppBar";
 
 /**
  * LandingPage is a React component that renders the landing page of the application.
@@ -23,42 +24,43 @@ export default function LandingPage() {
     return (
         <Box>
             {/* AppBar */}
-            <AppBar position="static" color="transparent" elevation={0}>
-                <Toolbar>
-                    {/* Logo */}
-                    <img
-                        src="/favicon.png"
-                        alt="UniPath.io Logo"
-                        style={{ height: "50px", marginRight: "10px" }}
-                    />
-                    <Typography
-                        variant="h6"
-                        noWrap
-                        sx={{
-                            mr: 2,
-                            display: { xs: "none", md: "flex" },
-                        }}
-                    >
-                        UniPath.io
-                    </Typography>
+            <AppBarComponent isLandingPage={true} />
+            {/*<AppBar position="static" color="transparent" elevation={0}>*/}
+            {/*    <Toolbar>*/}
+            {/*        /!* Logo *!/*/}
+            {/*        <img*/}
+            {/*            src="/favicon.png"*/}
+            {/*            alt="UniPath.io Logo"*/}
+            {/*            style={{ height: "50px", marginRight: "10px" }}*/}
+            {/*        />*/}
+            {/*        <Typography*/}
+            {/*            variant="h6"*/}
+            {/*            noWrap*/}
+            {/*            sx={{*/}
+            {/*                mr: 2,*/}
+            {/*                display: { xs: "none", md: "flex" },*/}
+            {/*            }}*/}
+            {/*        >*/}
+            {/*            UniPath.io*/}
+            {/*        </Typography>*/}
 
-                    {/* This pushes everything after it to the right */}
-                    <Box sx={{ flexGrow: 1 }} />
+            {/*        /!* This pushes everything after it to the right *!/*/}
+            {/*        <Box sx={{ flexGrow: 1 }} />*/}
 
-                    {/* Buttons */}
-                    <Box>
-                        <Button color="inherit" href="/home">
-                            About
-                        </Button>
-                        <Button color="inherit" href="/signin">
-                            Login
-                        </Button>
-                        <Button color="inherit" href="/signup">
-                            Sign Up
-                        </Button>
-                    </Box>
-                </Toolbar>
-            </AppBar>
+            {/*        /!* Buttons *!/*/}
+            {/*        <Box>*/}
+            {/*            <Button color="inherit" href="/home">*/}
+            {/*                About*/}
+            {/*            </Button>*/}
+            {/*            <Button color="inherit" href="/signin">*/}
+            {/*                Login*/}
+            {/*            </Button>*/}
+            {/*            <Button color="inherit" href="/signup">*/}
+            {/*                Sign Up*/}
+            {/*            </Button>*/}
+            {/*        </Box>*/}
+            {/*    </Toolbar>*/}
+            {/*</AppBar>*/}
 
             {/* Hero Section */}
             <Box sx={{ p: { xs: 3, sm: 5, md: 10 } }}>
