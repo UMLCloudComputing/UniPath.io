4a8c9645cfadc8ea0b234f3040625464fbbfee10
Nick Bottari <81245965+nbottari9@users.noreply.github.com>
2024-01-25 23:13:58 -0500

    Sign in page (#102)
    * Sign in page now properly authenticates with AWS.
    
    I added an 'incorrect password' message but I cant do CSS for my life
    
    * Sign in page now properly authenticates with AWS.
    
    I added an 'incorrect password' message but I cant do CSS for my life
    
    * refactored app bar into a single component and added condition to only show certain elements on certain pages
    
    * rewrote sign in page to use AWS Authenticator component. it also properly reroutes after the sign-in to the home page
    
    * moved AppBar.js to src/components so we can reuse it for other pages
    
    ---------
    
    Co-authored-by: ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>

--- src/app/(home)/layout.js
+++ src/app/(home)/layout.js
@@ -1,18 +1,18 @@
 "use client";
 
 // React
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 // Material UI
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 // Local
-import AppBar from './AppBar'; // Import AppBar component
-import Drawer from './Drawer'; // Import Drawer component
-import BottomNavigation from './BottomNavigation';
+import AppBar from "../../components/AppBar"; // Import AppBar component
+import Drawer from "./Drawer"; // Import Drawer component
+import BottomNavigation from "./BottomNavigation";
 
 // Global Variables
 const drawerWidth = 240;
@@ -28,9 +28,8 @@ const drawerWidthClosed = 100;
  * @returns {ReactElement} The React Element created by this function.
  */
 export default function Layout({ children }) {
-
     const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('md'));   // Using a media query to determine screen size
+    const isMobile = useMediaQuery(theme.breakpoints.down("md")); // Using a media query to determine screen size
     const [open, setOpen] = useState(!isMobile);
 
     const handleDrawerOpen = () => {
@@ -39,16 +38,17 @@ export default function Layout({ children }) {
 
     const handleDrawerClose = () => {
         setOpen(false);
-        console.log('handleDrawerClose');
+        console.log("handleDrawerClose");
     };
 
     return (
-        <Box sx={{ 
-            display: 'flex',
-            backgroundColor: theme.palette.action.hover,
-            minHeight: '100vh',
-        }}>
-
+        <Box
+            sx={{
+                display: "flex",
+                backgroundColor: theme.palette.action.hover,
+                minHeight: "100vh",
+            }}
+        >
             {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
             <CssBaseline />
 
@@ -57,35 +57,40 @@ export default function Layout({ children }) {
                 drawerWidth={drawerWidth}
                 open={open}
                 handleDrawerOpen={handleDrawerOpen}
+                isLandingPage={false}
             />
-            
+
             {/* Main */}
-            <Box component="main" sx={{ 
-                flexGrow: 1,
-                p: 3,
-                width: { 
-                    xs: '100%',
-                    md: `calc(100% - ${open ? drawerWidth : drawerWidthClosed}px)` 
-                },
-                transition: (theme) =>
-                    theme.transitions.create('margin', {
-                        easing: theme.transitions.easing.sharp,
-                        duration: theme.transitions.duration.leavingScreen,
-                    }),
-                ...(open && {
-                    ml: {
-                        xs: 0,
-                        md: `${drawerWidth}px`,
+            <Box
+                component="main"
+                sx={{
+                    flexGrow: 1,
+                    p: 3,
+                    width: {
+                        xs: "100%",
+                        md: `calc(100% - ${
+                            open ? drawerWidth : drawerWidthClosed
+                        }px)`,
                     },
                     transition: (theme) =>
-                        theme.transitions.create('margin', {
-                            easing: theme.transitions.easing.easeOut,
-                            duration: theme.transitions.duration.enteringScreen,
+                        theme.transitions.create("margin", {
+                            easing: theme.transitions.easing.sharp,
+                            duration: theme.transitions.duration.leavingScreen,
                         }),
-                }),
-
-            }}>
-
+                    ...(open && {
+                        ml: {
+                            xs: 0,
+                            md: `${drawerWidth}px`,
+                        },
+                        transition: (theme) =>
+                            theme.transitions.create("margin", {
+                                easing: theme.transitions.easing.easeOut,
+                                duration:
+                                    theme.transitions.duration.enteringScreen,
+                            }),
+                    }),
+                }}
+            >
                 {/* Sidebar Drawer */}
                 <Drawer
                     drawerWidth={drawerWidth}
@@ -95,14 +100,11 @@ export default function Layout({ children }) {
                 />
 
                 {/* Children */}
-                <Box sx={{ mt: 7 }}>
-                    {children}
-                </Box>
+                <Box sx={{ mt: 7 }}>{children}</Box>
 
                 {/* Bottom Navigation */}
                 <BottomNavigation />
-
             </Box>
-        </Box >
+        </Box>
     );
 }
