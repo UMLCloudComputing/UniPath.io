9a0e6a69cd4dc0645bb764f1620d56d8c2472614
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2024-01-25 22:21:04 -0500

    rewrote sign in page to use AWS Authenticator component. it also properly reroutes after the sign-in to the home page

--- src/app/(auth)/signin/page.js
+++ src/app/(auth)/signin/page.js
@@ -18,13 +18,14 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 
 // Next.js
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
 
 // AWS
-import { signIn } from "aws-amplify/auth";
+import {signIn} from "aws-amplify/auth";
+import {Authenticator, useAuthenticator} from "@aws-amplify/ui-react";
 
 function Copyright(props) {
     return (
@@ -49,117 +50,17 @@ const defaultTheme = createTheme();
 // Page: SignInPage;
 export default function SignInPage() {
     const router = useRouter();
-    const [showIncorrectEmailOrPassword, setShowIncorrectEmailOrPassword] =
-        React.useState(false);
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get("email"),
-            password: data.get("password"),
-        });
-        try {
-            const { isSignedIn, nextStep } = await signIn({
-                username: data.get("email"),
-                password: data.get("password"),
-            });
-            switch (
-                nextStep.signInStep // this is where we could reroute in the future to SMS confirmation codes or password reset, etc.
-            ) {
-                case "DONE":
-                    router.push("/home");
-            }
-        } catch ({ name, message }) {
-            console.log("error signing in", name, message);
 
-            if (name === "NotAuthorizedException") {
-                setShowIncorrectEmailOrPassword(true);
-            }
+    const {route} = useAuthenticator((context) => [context.route]);
+    React.useEffect(() => {
+        if (route === "authenticated") {
+            router.replace("/home");
         }
-    };
+    }, [route]);
 
     return (
-        <ThemeProvider theme={defaultTheme}>
-            <Container component="main" maxWidth="xs">
-                <CssBaseline />
 
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                    }}
-                >
-                    <Avatar sx={{ m: 1, bgcolor: "secondary.main " }}>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Sign in
-                    </Typography>
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        noValidate
-                        sx={{ mt: 1 }}
-                    >
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        {showIncorrectEmailOrPassword ? (
-                            <Box
-                                margin="normal"
-                                id="email-or-pass-error"
-                                name="email-or-pass-error"
-                                color={"red"}
-                            >
-                                Incorrect email or password
-                            </Box>
-                        ) : null}
-                        <FormControlLabel
-                            control={
-                                <Checkbox value="remember" color="primary" />
-                            }
-                            label="Remember me"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign in
-                        </Button>
-                        <Grid container>
-                            <Link href="#" variant="body2">
-                                Forgot Password?
-                            </Link>
-                        </Grid>
-                        <Grid item>
-                            <Link href="/signup" variant="body2">
-                                {"Don't have an account? Sign Up"}
-                            </Link>
-                        </Grid>
-                    </Box>
-                </Box>
-            </Container>
-        </ThemeProvider>
+        <Authenticator/>
+
     );
 }
