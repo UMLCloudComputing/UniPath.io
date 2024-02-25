cd1ba385ea82994e003cc8581590d84065291e42
ComposedNebula <81245965+ComposedNebula@users.noreply.github.com>
2024-02-25 14:40:12 -0500

    fixing merge conflicts

--- src/app/(home)/pathways/components/PathwayCard.js
+++ src/app/(home)/pathways/components/PathwayCard.js
@@ -5,7 +5,7 @@
 
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
+import {useTheme} from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import RouteIcon from '@mui/icons-material/Route';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -21,11 +21,19 @@ import React from 'react';
 import MenuItem from "@mui/material/MenuItem";
 
 
-
-const PathwayCard = ({ pathwayId, pathwayTitle, school, degreeType, yearOfGrad, degreeMajor, handlePathwayEdit, handlePathwayDelete}) => {
-  const theme = useTheme();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const pathwayOptionsOpen = Boolean(anchorEl);
+export default function PathwayCard ({
+                         pathwayId,
+                         pathwayTitle,
+                         school,
+                         degreeType,
+                         yearOfGrad,
+                         degreeMajor,
+                         handlePathwayEdit,
+                         handlePathwayDelete
+                     }) {
+    const theme = useTheme();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const pathwayOptionsOpen = Boolean(anchorEl);
 
     const handlePathwayClick = () => { //to be implemented
         console.log('redirect to pathway page');
@@ -42,145 +50,132 @@ const PathwayCard = ({ pathwayId, pathwayTitle, school, degreeType, yearOfGrad, 
     }
 
 
-
-
-  const pathwayOptionsMenu = (
-      <Menu
-          anchorEl={anchorEl}
-          open={pathwayOptionsOpen}
-          onClose={handlePathwayOptionsClose}
-          anchorOrigin={{
-          vertical: "top",
-          horizontal: "right",
-      }}
-
-          keepMounted
-          transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-      }}>
-          <MenuItem onClick={() => {
-              handlePathwayEdit(pathwayId)
-              handlePathwayOptionsClose()
-          }}>Edit</MenuItem>
-          <MenuItem
-              onClick={() => handlePathwayDelete(pathwayId)}
-              sx={{
-                  color: theme.palette.error.main
-              }}
-          >Delete</MenuItem>
-
-      </Menu>
-  )
-
-  return (
-
-
-
-const PathwayCard = ({ degreeTitle, school, degreeType, yearOfGrad, degreeMajor, handlePathwayClick, handlePathwayDelete }) => {
-  const theme = useTheme();
-  return (
-    <>
-
-      <Card
-        sx={{
-          width: '35%',
-          p: 2,
-            borderRadius: 3,
-        }}
-      >
-          <Box
-          sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              gap: 0.5,
-          }}>
-        <Box
-          sx={{
-            display: 'flex',
-              alignItems: 'center'
-          }}
-        >
-          <PathIcon
-            sx={{
-              color: theme.palette.grey[500],
-            }} />
-
-
-          <Typography
-            variant="h5"
-            sx={{
-              ml: 1
+    const pathwayOptionsMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            open={pathwayOptionsOpen}
+            onClose={handlePathwayOptionsClose}
+            anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
             }}
-          >
-            {pathwayTitle}
-          </Typography>
-            <IconButton
-                sx={{
-                ml:'auto'
-                    }}
-            onClick={handlePathwayOptionsOpen}>
-          <MoreVertIcon/>
-        </IconButton>
 
-            {degreeTitle}
-          </Typography>
-
-
-          
-
-      </Box>
-          <Typography
-              variant={"h6"}
-              sx={{
-                  color: theme.palette.grey[500],
-                  fontSize: 14,
-              }}>
-              {degreeType}, {degreeMajor}
-          </Typography>
-          </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            mt: 4,
-          }}
-        >
-            <Box
-            sx={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: 0.5,
-
+            keepMounted
+            transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
             }}>
-          <SchoolIcon
-            sx={{
-              color: theme.palette.grey[500],
-            }} />
-          <Typography
-            variant="subtitle2"
-            fontWeight="regular"
-            sx={{
-              ml: 1,
-              color: theme.palette.grey[700],
-            }}
-          >{school}, {yearOfGrad}</Typography>
-            </Box>
+            <MenuItem onClick={() => {
+                handlePathwayEdit(pathwayId)
+                handlePathwayOptionsClose()
+            }}>Edit</MenuItem>
+            <MenuItem
+                onClick={() => handlePathwayDelete(pathwayId)}
+                sx={{
+                    color: theme.palette.error.main
+                }}
+            >Delete</MenuItem>
 
-            <IconButton
-            sx={{
-                ml: 'auto',
-            }}
-            onClick={handlePathwayClick}>
-          <ArrowForwardIcon
-            sx={{
+        </Menu>
+    )
 
-              color: theme.palette.primary.main,
-            }} />
-            </IconButton>
-        </Box>
-          {pathwayOptionsMenu}
-      </Card>
-  );
-};
 
-export default PathwayCard;
+    return (
+        <>
+
+            <Card
+                sx={{
+                    width: '35%',
+                    p: 2,
+                    borderRadius: 3,
+                }}
+            >
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 0.5,
+                    }}>
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center'
+                        }}
+                    >
+                        <PathIcon
+                            sx={{
+                                color: theme.palette.grey[500],
+                            }}/>
+
+
+                        <Typography
+                            variant="h5"
+                            sx={{
+                                ml: 1
+                            }}
+                        >
+                            {pathwayTitle}
+                        </Typography>
+                        <IconButton
+                            sx={{
+                                ml: 'auto'
+                            }}
+                            onClick={handlePathwayOptionsOpen}>
+                            <MoreVertIcon/>
+                        </IconButton>
+
+
+                    </Box>
+                    <Typography
+                        variant={"h6"}
+                        sx={{
+                            color: theme.palette.grey[500],
+                            fontSize: 14,
+                        }}>
+                        {degreeType}, {degreeMajor}
+                    </Typography>
+                </Box>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        mt: 4,
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            gap: 0.5,
+
+                        }}>
+                        <SchoolIcon
+                            sx={{
+                                color: theme.palette.grey[500],
+                            }}/>
+                        <Typography
+                            variant="subtitle2"
+                            fontWeight="regular"
+                            sx={{
+                                ml: 1,
+                                color: theme.palette.grey[700],
+                            }}
+                        >{school}, {yearOfGrad}</Typography>
+                    </Box>
+
+                    <IconButton
+                        sx={{
+                            ml: 'auto',
+                        }}
+                        onClick={handlePathwayClick}>
+                        <ArrowForwardIcon
+                            sx={{
+
+                                color: theme.palette.primary.main,
+                            }}/>
+                    </IconButton>
+                </Box>
+                {pathwayOptionsMenu}
+            </Card>
+            </>
+)
+            }
