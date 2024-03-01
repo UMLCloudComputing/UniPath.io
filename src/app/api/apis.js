6c7cc93558bd38e676748d722326f045b2ca95b5
Gurpranked <gurpreet.singh.rehal@outlook.com>
2024-03-01 13:12:45 -0500

    Created API Endpoint Files

---
+++ src/app/api/apis.js
@@ -0,0 +1,10 @@
+
+
+export default function handler(req, res) {
+    res.status(200).json({message:'Hello from Massachusetts!'})
+}
+
+
+export default function handler(req: NextApiRequest, res: NextApiRequest){
+    
+}
