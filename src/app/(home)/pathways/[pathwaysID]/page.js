56f51b666ceee0a2464a90b2d02a5429e6a56883
RoshanKarthikRajesh <myhorsefly12345@gmail.com>
2024-03-06 12:10:53 -0500

    Added mockAccordionApiCall function to mockData.js and added some stuff to the pathways page

---
+++ src/app/(home)/pathways/[pathwaysID]/page.js
@@ -0,0 +1,39 @@
+"use client";
+
+// React stuff
+import { useState, useEffect } from "react";
+
+// Local 
+import SemesterAccordion from "@/components/Accordions/SemesterAccordion";
+import { mockAccordionApiCall, mockPathwayApiCall } from "@/components/Data/mockData";
+
+export default function degreePathwayPage ()
+{
+    const [semesters, setSemesters] = useState([]);
+
+    useEffect(() =>
+    {
+        mockAccordionApiCall()
+            .then((semesters) =>
+            {
+                setSemesters(semesters);
+            })
+            .catch((err) => console.log('mockAccordionApiCall' + err))
+    }, []);
+
+    return (
+        <>
+        {
+            semesters.map((semester) =>
+            {
+                return (
+                    <SemesterAccordion
+                        title={semester.title}
+                        rows={semester.rows}
+                    />
+                )
+            })
+        }
+        </>
+    );
+}
