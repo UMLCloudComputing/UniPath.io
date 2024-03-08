7606782185e5da5579a9b187505bda66f032c953
Noah <noah.shayne@gmail.com>
2024-03-08 02:01:36 -0500

    Testing

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css"; // Amplify component styles
 
 // Material UI
-import { ThemeProvider } from '@mui/material/styles';
+//import { ThemeProvider } from '@mui/material/styles';
 import Box from "@mui/material/Box";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -16,14 +16,13 @@ import CssBaseline from "@mui/material/CssBaseline";
 import ConfigureAmplifyClientSide from "@/components/ConfigureAmplify";
 import { darkTheme, lightTheme } from "@/components/theme";
 import { ThemeContext, ThemeContextProvider } from "@/contexts/ThemeContext";
-
+import {Providers} from "@/components/AppBar/Providers";
 
 // MainApp is the main high-level layout component that wraps around other components in this application.
 const MainApp = ({ children }) => {
-    const { darkMode } = React.useContext(ThemeContext);
+   // const { darkMode } = React.useContext(ThemeContext);
 
     return (
-        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
             <Authenticator.Provider>
                 <View>
                     <CssBaseline/>
@@ -32,7 +31,7 @@ const MainApp = ({ children }) => {
                     </Box>
                 </View>
             </Authenticator.Provider>
-        </ThemeProvider>
+
     );
 };
 
@@ -40,11 +39,12 @@ const MainApp = ({ children }) => {
 // It provides them with theme and authentication contexts.
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
             <head>
                 <link rel="icon" href="/favicon.png" />
             </head>
             <body>
+                <Providers>
                 <AppRouterCacheProvider>
                     <ConfigureAmplifyClientSide />
                     <ThemeContextProvider>
@@ -53,6 +53,7 @@ export default function RootLayout({ children }) {
                         </MainApp>
                     </ThemeContextProvider>
                 </AppRouterCacheProvider>
+                </Providers>
             </body>
         </html>
     );
--- src/components/AppBar/DarkModeToggle.js
+++ src/components/AppBar/DarkModeToggle.js
@@ -1,18 +1,34 @@
-import { DarkMode, DarkModeOutlined } from "@mui/icons-material";
-import IconButton from "@mui/material/IconButton";
-import * as React from "react";
-import { ThemeContext } from "../../contexts/ThemeContext";
+'use client'
 
-export default function DarkModeToggle() {
-    const { darkMode, toggleTheme } = React.useContext(ThemeContext);
+import { useEffect, useState } from 'react'
+import { useTheme } from 'next-themes'
 
-    return (
-        <IconButton onClick={toggleTheme}>
-            {darkMode ? (
-                <DarkMode color={"inherit"} />
-            ) : (
-                <DarkModeOutlined color={"inherit"} />
-            )}
-        </IconButton>
-    );
+import { SunIcon, MoonIcon } from '@heroicons/react/24/solid'
+
+const DarkModeToggle = () => {
+  const { resolvedTheme, setTheme } = useTheme()
+
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
+  if (!mounted) {
+    return null
+  }
+
+  return (
+    <button
+      aria-label='Toggle Dark Mode'
+      type='button'
+      className='flex items-center justify-center rounded-lg p-2 transition-colors hover:bg-zinc-100 dark:hover:bg-zinc-700'
+      onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+    >
+      {resolvedTheme === 'dark' ? (
+        <SunIcon className='h-5 w-5 text-orange-300' />
+      ) : (
+        <MoonIcon className='h-5 w-5 text-slate-800' />
+      )}
+    </button>
+  )
 }
+
+export default DarkModeToggle
