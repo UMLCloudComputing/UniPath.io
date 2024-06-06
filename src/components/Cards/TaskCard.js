15d1630df4d8b0af7a3cf01352be53746da066cd
Houston_Boston <myhorsefly12345@gmail.com>
2024-06-06 20:28:38 +0530

    Made it look more like the Google Tasks layout. Have to implement add task and edit functionalities.

--- src/components/Cards/TaskCard.js
+++ src/components/Cards/TaskCard.js
@@ -11,23 +11,37 @@ import Button from '@mui/material/Button';
 // React
 import React from 'react'
 import { useTheme } from '@mui/material/styles';
-import { Tab } from '@mui/material';
+import { MoreVertOutlined } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 
 export default function TaskCard() {
     const theme = useTheme();
     return (
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-            <Card sx={{ width: '50%', borderRadius: '20px'}}>
-                <CardActions>
-                    <Button>
-                        <AddTaskIcon color='blue' />
-                        <Box width={'5px'} />
-                        <Typography>
-                            Add Task
+        <>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card sx={{ width: '50%', borderRadius: '20px' }}>
+                    <div style={{ display: 'flex', flexDirection:'row', width:'100%' }}>
+                        <Typography variant='h4' sx={{ marginLeft: '2%' }}>
+                            My Tasks
                         </Typography>
-                    </Button>
-                </CardActions>
-            </Card>
-        </div>
+
+                        <IconButton style={{ marginLeft:'auto', marginRight:'2%' }}>
+                            <MoreVertOutlined />
+                        </IconButton>
+                    </div>
+
+                    <CardActions sx={{ width: '100%' }}>
+                        <Button sx={{ textTransform: 'inherit', width: '100%', justifyContent: 'left', borderRadius: '20px', }}>
+                            <AddTaskIcon color='blue' />
+                            <Box width={'25px'} />
+                            <Typography>
+                                Add a task
+                            </Typography>
+                        </Button>
+                    </CardActions>
+
+                </Card>
+            </div>
+        </>
     );
 }
