d29bbe1e32d10cea2c854721eaa63cd6255ef6a8
Houston_Boston <myhorsefly12345@gmail.com>
2024-06-06 17:22:09 +0530

    Added the add task icon as well as made the card centered.

--- src/components/Cards/TaskCard.js
+++ src/components/Cards/TaskCard.js
@@ -4,15 +4,30 @@ import Box from '@mui/material/Box';
 import AddTaskIcon from '@mui/icons-material/AddTask';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 
 // React
 import React from 'react'
+import { useTheme } from '@mui/material/styles';
+import { Tab } from '@mui/material';
 
-export default function TaskCard(){
-    return(
-        <Card>
-            <AddTaskIcon></AddTaskIcon> <CardActions>Add Task</CardActions>
-        </Card>
+export default function TaskCard() {
+    const theme = useTheme();
+    return (
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+            <Card sx={{ width: '50%' }}>
+                <CardActions>
+                    <Button>
+                        <AddTaskIcon color='blue' />
+                        <Box width={'5px'} />
+                        <Typography>
+                            Add Task
+                        </Typography>
+                    </Button>
+                </CardActions>
+            </Card>
+        </div>
     );
 }
