aeed4f653b280e32abc490c9ca533ddb3e202f23
Houston_Boston <myhorsefly12345@gmail.com>
2024-06-07 13:10:45 +0530

    Started working on the task input aspect of the implementation.

--- src/components/Cards/TaskCard.js
+++ src/components/Cards/TaskCard.js
@@ -1,45 +1,22 @@
 "use client";
-// MUI
-import Box from '@mui/material/Box';
-import AddTaskIcon from '@mui/icons-material/AddTask';
+//MUI
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Radio from '@mui/material/Radio';
 
-
-// React
-import React from 'react'
-import { useTheme } from '@mui/material/styles';
-import { MoreVertOutlined } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+//React
+import React from 'react';
 
 export default function TaskCard() {
-    const theme = useTheme();
+
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <Card sx={{ width: '50%', borderRadius: '20px' }}>
-                    <div style={{ display: 'flex', flexDirection:'row', width:'100%' }}>
-                        <Typography variant='h4' sx={{ marginLeft: '2%' }}>
-                            My Tasks
-                        </Typography>
-
-                        <IconButton style={{ marginLeft:'auto', marginRight:'2%' }}>
-                            <MoreVertOutlined />
-                        </IconButton>
+            <div style={{ justifyContent: 'center', display: 'flex' }}>
+                <Card sx={{ width: '50%', borderRadius: '0px' }}>
+                    <div style={{ justifyContent: 'left', display: 'flex', alignItems:'center' }}>
+                        <Radio size='small' />
+                        <TextField placeholder='Title' variant='standard' sx={{ width: '100%' }} InputProps={{ disableUnderline: 'true' }} />
                     </div>
-
-                    <CardActions sx={{ width: '100%' }}>
-                        <Button sx={{ textTransform: 'inherit', width: '100%', justifyContent: 'left', borderRadius: '20px', }}>
-                            <AddTaskIcon color='blue' />
-                            <Box width={'25px'} />
-                            <Typography>
-                                Add a task
-                            </Typography>
-                        </Button>
-                    </CardActions>
-
                 </Card>
             </div>
         </>
---
+++ src/components/Cards/TaskHeaderCard.js
@@ -0,0 +1,47 @@
+"use client";
+// MUI
+import Box from '@mui/material/Box';
+import AddTaskIcon from '@mui/icons-material/AddTask';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import MoreVertOutlined from '@mui/icons-material/MoreVertOutlined';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+
+// React
+import React from 'react';
+
+export default function TaskHeaderCard() {
+    return (
+        <>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <Card sx={{ width: '50%', borderTopRightRadius: '20px', borderTopLeftRadius: '20px' }}>
+                    <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'left' }}>
+                        <Typography variant='h4' sx={{ marginLeft: '2%' }}>
+                            My Tasks
+                        </Typography>
+
+                        <IconButton style={{ marginLeft: 'auto', marginRight: '2%', top: '3px', position: 'relative' }}>
+                            <Tooltip title='More options' arrow>
+                                <MoreVertOutlined />
+                            </Tooltip>
+                        </IconButton>
+                    </div>
+
+                    <CardActions sx={{ width: '100%' }}>
+                        <Button sx={{ textTransform: 'inherit', width: '100%', justifyContent: 'left', borderRadius: '20px', }}>
+                            <AddTaskIcon color='blue' />
+                            <Box width={'25px'} />
+                            <Typography>
+                                Add a task
+                            </Typography>
+                        </Button>
+                    </CardActions>
+
+                </Card>
+            </div>
+        </>
+    );
+}
