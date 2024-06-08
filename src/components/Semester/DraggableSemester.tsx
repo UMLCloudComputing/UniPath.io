32f738b3c2d433308afddfbdb6e3a09ce4a65cc3
ComposedNebula <81245965+nbottari9@users.noreply.github.com>
2024-06-07 20:43:01 -0400

    fix::type errors

--- src/components/Semester/DraggableSemester.tsx
+++ src/components/Semester/DraggableSemester.tsx
@@ -3,28 +3,29 @@ import { Semester } from "./Semester"
 import React from "react"
 import { Box } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2"
+import { CourseType, SemesterType } from "../../types/types"
 
-export const DraggableSemester = ({ title, courses, index }: { title: string, courses: Course[], index: number }) => {
-    console.log(index)
+export const DraggableSemester = ({ title, courses, id }: SemesterType) => {
     return (
-        <Grid md={6} sx={{ minWidth: "40%" }}>
-            <Draggable
-                draggableId={title}
-                index={index}
-            >
-                {
-                    (provided) => (
-                        <Box
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                        >
-                            <Semester title={title} _courses={courses} />
-                        </Box>
-                    )
-                }
+        // <Grid md={6} sx={{ minWidth: "40%" }}>
+        //     <Draggable
+        //         draggableId={title}
+        //         index={index}
+        //     >
+        //         {
+        //             (provided) => (
+        //                 <Box
+        //                     ref={provided.innerRef}
+        //                     {...provided.draggableProps}
+        //                     {...provided.dragHandleProps}
+        //                 >
+        //                     <Semester id={id} title={title} courses={courses} />
+        //                 </Box>
+        //             )
+        //         }
 
-            </Draggable>
-        </Grid>
+        //     </Draggable>
+        // </Grid>
+        <></>
     )
 }
