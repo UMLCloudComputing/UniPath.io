b12d647e1638452e72893f1e1cfd57e21fd99585
RoshanKarthikRajesh <myhorsefly12345@gmail.com>
2024-06-19 12:06:58 -0400

    Added date/time icon and text. Need to change the button's highlight color.

--- src/components/Cards/TaskCard.js
+++ src/components/Cards/TaskCard.js
@@ -7,30 +7,39 @@ import IconButton from '@mui/material/IconButton';
 import NotesIcon from '@mui/icons-material/Notes';
 import List from '@mui/material/List';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
 
 //React
 import React from 'react';
 
-export default function TaskCard() {
+export default function TaskCard ()
+{
 
     return (
         <>
             <div style={{ justifyContent: 'center', display: 'flex' }}>
-                
+
                 <Card sx={{ width: '50%', borderRadius: '0px' }}>
-                    
+
                     <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
                         <Radio size='small' />
                         <TextField placeholder='Title' variant='standard' sx={{ width: '100%' }} InputProps={{ disableUnderline: 'true' }} />
                     </div>
-                    
+
                     <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
                         <NotesIcon sx={{ marginLeft: '1.1%' }} />
                         <TextField placeholder='Details' variant='standard' sx={{ width: '100%', marginLeft: '1%' }} InputProps={{ disableUnderline: 'true' }} />
                     </div>
-                    
-                    <div style={{ justifyContent: 'left', display:'flex', alignItems:'center'}}>
-                        <CalendarMonthIcon />
+
+                    <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
+                        <Button sx={{textTransform:'inherit', width:'100%', justifyContent:'left', borderRadius:'20px', color:'gray'}}>
+                            <CalendarMonthIcon sx={{ color: 'gray' }} />
+                            <Typography variant="body1" color="GrayText" sx={{ marginLeft: '1%' }}>
+                                Date/time
+                            </Typography>
+                        </Button>
                     </div>
                 </Card>
             </div>
