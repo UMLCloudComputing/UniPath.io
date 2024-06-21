70bd3340e41ec911dae965a397aad1700c2bb9ce
houston_boston <50076471+HoustonBoston@users.noreply.github.com>
2024-06-21 11:13:29 -0400

    Added stuff from my older branch (#200)

--- src/components/Cards/TaskCard.js
+++ src/components/Cards/TaskCard.js
@@ -3,19 +3,41 @@
 import Card from '@mui/material/Card';
 import TextField from '@mui/material/TextField';
 import Radio from '@mui/material/Radio';
+import NotesIcon from '@mui/icons-material/Notes';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
 
 //React
 import React from 'react';
 
-export default function TaskCard() {
+export default function TaskCard ()
+{
 
     return (
         <>
             <div style={{ justifyContent: 'center', display: 'flex' }}>
+
                 <Card sx={{ width: '50%', borderRadius: '0px' }}>
-                    <div style={{ justifyContent: 'left', display: 'flex', alignItems:'center' }}>
+
+                    <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
                         <Radio size='small' />
                         <TextField placeholder='Title' variant='standard' sx={{ width: '100%' }} InputProps={{ disableUnderline: 'true' }} />
+                    </div>
+
+                    <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
+                        <NotesIcon sx={{ marginLeft: '1.1%' }} />
+                        <TextField placeholder='Details' variant='standard' sx={{ width: '100%', marginLeft: '1%' }} InputProps={{ disableUnderline: 'true' }} />
+                    </div>
+
+                    <div style={{ justifyContent: 'left', display: 'flex', alignItems: 'center' }}>
+                        <Button sx={{textTransform:'inherit', width:'100%', justifyContent:'left', borderRadius:'20px', color:'gray'}}>
+                            <CalendarMonthIcon sx={{ color: 'gray' }} />
+                            <Typography variant="body1" color="GrayText" sx={{ marginLeft: '1%' }}>
+                                Date/time
+                            </Typography>
+                        </Button>
                     </div>
                 </Card>
             </div>
