9bd02a9c90211d699d14cf00daf7e445f039e2b1
RoshanKarthikRajesh <myhorsefly12345@gmail.com>
2024-06-28 10:04:12 -0400

    Started on the date picker dialog. Needs more work.

---
+++ src/components/Dialogs/DatePickerDialog.js
@@ -0,0 +1,15 @@
+//React
+import * as React from 'react';
+
+//MUI
+import { Dialog } from "@mui/material";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+export default function DatePickerDialog ()
+{
+    return (
+        <Dialog>
+            
+        </Dialog>
+    )
+} 
